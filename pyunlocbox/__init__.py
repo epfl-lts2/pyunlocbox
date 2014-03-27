694c803757a00879e7eb03fcd4a8470f1c892300
Michaël Defferrard <michael.defferrard@epfl.ch>
2014-03-27 16:41:19 +0100

    Add package documentation

--- pyunlocbox/__init__.py
+++ pyunlocbox/__init__.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+PyUNLockBox is a convex optimization toolbox using proximal splitting methods.
+It is a port of the Matlab UNLocBox toolbox.
+The pyunlockbox package is divided into the following modules :
+
+* :mod:`solvers`: problem solvers
+* :mod:`functions`: functions to be passed to the solvers
+* :mod:`demos`: some problem solving demonstrations using the toolbox
+"""
+
 __author__ = 'EPFL LTS2'
 __email__ = 'nathanael.perraudin@epfl.ch'
 __version__ = '0.1.0'
