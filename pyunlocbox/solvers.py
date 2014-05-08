c3f5b5cc54c358c5fe19bfbcc31942c4b61e3a2d
Michaël Defferrard <michael.defferrard@epfl.ch>
2014-05-08 18:06:16 +0200

    Forward-backward core code

--- pyunlocbox/solvers.py
+++ pyunlocbox/solvers.py
@@ -8,6 +8,8 @@ It includes the following solvers :
 * :func:`forward_backward`: Forward-backward splitting algorithm
 """
 
+import numpy as np
+
 
 def forward_backward(x0, f1, f2, gamma=1, reltol=10**-3, abstol=0,
                      method='FISTA', lamb=1, maxit=200, verbosity='low'):
@@ -59,3 +61,52 @@ def forward_backward(x0, f1, f2, gamma=1, reltol=10**-3, abstol=0,
         raise ValueError('lamb is bounded by 0 and 1')
     if verbosity != 'none' or verbosity != 'low' or verbosity != 'high':
         raise ValueError('verbosity should be either none, low or high')
+
+    curNorm = f1.eval(x0) + f2.eval(x0)
+# convergence test
+    stop = False
+
+    if verbosity == 'high':
+        print('Selected algorithm' + str(method))
+
+    # ISTA initialization.
+    xn = x0
+
+    # FISTA initialization.
+    un = x0
+    sol = x0
+    tn = 1.
+
+    # Iterative ISTA or FISTA algorithm.
+    while !stop:
+
+        if verbosity == 'high':
+            print('Iteration ' + str(iter))
+
+        if method == 'ISTA':
+            yn = xn - gamma * f2.grad(xn)
+            sol = xn + lamb * (f1.prox(yn, gamma) - xn)
+            xn = sol
+        elif method == 'FISTA':
+            xn = f1.prox(un - gamma * f2.grad(un), gamma)
+            tn1 = (1. + np.sqrt(1.+4.*tn^2.)) / 2.
+            un = xn + (tn-1) / tn1 * (xn-sol)
+            sol = xn
+            tn = tn1
+        else:
+            raise ValueError('method should be FISTA or ISTA')
+
+        curNorm = f1.eval(sol) + f2.eval(sol)
+# convergence test
+# post process
+        if verbosity == 'high':
+            print('||f1 + f2|| = ' + str(curNorm) +
+                  ', relative norm : ' + str(relNorm))
+
+    # Final logging.
+    if verbosity == 'high' or verbosity == 'low':
+        print('Solution found (in ' + str(iter) + ' iterations) : ||f|| = ' +
+              str(curNorm) + ', relative norm : ' + str(relNorm) +
+              ', stopping criterion : ' + str(crit))
+
+# info struct
