02a2e2f24628d8a52684db44246331be47c9e36d
Michaël Defferrard <michael.defferrard@epfl.ch>
2014-05-09 15:12:08 +0200

    Comments and messages update

--- pyunlocbox/functions.py
+++ pyunlocbox/functions.py
@@ -3,11 +3,12 @@
 
 """
 This module implements function objects which are then passed to solvers.
-The *func* base class implements the interface whereas specialised classes
-who inherit from it implement the methods. These classes include :
+The :class:`func` base class implements the interface whereas specialised
+classes who inherit from it implement the methods. These classes include :
 
-* :class:`norm_l2`: L2-norm which implements ``eval``, ``prox`` and ``grad``
-* :class:`norm_l1`: L1-norm which implements ``eval`` and ``prox``
+* :class:`norm_l2`: L2-norm which implements :func:`eval`, :func:`prox`
+  and :func:`grad`
+* :class:`norm_l1`: L1-norm which implements :func:`eval` and :func:`prox`
 """
 
 import numpy as np
@@ -29,7 +30,7 @@ def _soft_threshold(z, T):
 
 class func:
     """
-    This class defines a function object to be passed to solvers. It is
+    This class defines a function object to be passed to :func:`solve`. It is
     intended to be a base class for standard functions which will implement
     the required methods. It can also be instantiated by user code and
     dynamically modified for rapid testing.
@@ -43,7 +44,7 @@ class func:
         Evaluation of the function at x.
         This method is used by all solvers.
         """
-        raise NotImplementedError("Class user should define this method")
+        raise NotImplementedError("Class user should define this method.")
 
     def prox(self, x, T):
         """
@@ -51,21 +52,21 @@ class func:
         :math:`prox_{f,\gamma}(x)=\min_z \\frac{1}{2} ||x-z||_2^2 +\gamma f(z)`
         This method is used by all proximal solvers.
         """
-        raise NotImplementedError("Class user should define this method")
+        raise NotImplementedError("Class user should define this method.")
 
     def grad(self, x):
         """
         Function gradient evaluated at x.
         This method is only used by some solvers.
         """
-        raise NotImplementedError("Class user should define this method")
+        raise NotImplementedError("Class user should define this method.")
 
 
 class norm(func):
     """
     Base class which defines the attributes of the norm objects.
 
-    Parameters :
+    Attributes :
 
     * *lamb*  : regularization parameter :math:`\lambda`
     * *w*     : weights for a weighted L2-norm (default :math:`w=1`)
@@ -120,7 +121,7 @@ class norm_l1(norm):
             sol = self.At(_solf_threshold(sol, gamma*self.nu*self.w) - sol)
             sol = x + sol / self.nu
         else:
-            raise NotImplementedError('Not implemented for non tight frame')
+            raise NotImplementedError('Not implemented for non tight frame.')
         return sol
 
 
@@ -148,7 +149,7 @@ class norm_l2(norm):
             sol = np.array(x) + 2. * gamma * self.At(self.y * self.w**2)
             sol /= 1. + 2. * gamma * self.nu * self.w**2
         else:
-            raise NotImplementedError('Not implemented for non tight frame')
+            raise NotImplementedError('Not implemented for non tight frame.')
         return sol
 
     def grad(self, x):
