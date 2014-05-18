6bcb48a52f8a695a07f74f22b394080fbaf66f2f
Michaël Defferrard <michael.defferrard@epfl.ch>
2014-05-18 06:29:21 +0200

    Convergence test. Many improvements in docstrings.

--- pyunlocbox/solvers.py
+++ pyunlocbox/solvers.py
@@ -1,179 +1,263 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-This module implements solvers which minimize functions output. Call
-:func:`solve` to solve your convex optimization problem using the solver
-and functions of your choice. The :class:`solver` class is a base class for all
-solvers. The following solvers are included :
+r"""
+This module implements solver objects who minimize an objective function. Call
+:func:`solve` to solve your convex optimization problem using your instanced
+solver and functions objects. The :class:`solver` base class defines the
+interface of all solver object. The specialized solver objects inherit from it
+and implement the class methods. The following solvers are included :
 
-* :class:`forward_backward`: Forward-backward splitting algorithm
+* :class:`forward_backward`: forward-backward proximal splitting algorithm
 """
 
 import numpy as np
+import time
 
 
-def solve(solver, f1, f2, x0):
-    """
-    Parameters :
+def solve(solver, f1, f2, x0, relTol=10**-3, absTol=0, maxIter=200,
+          verbosity='low'):
+    r"""
+    This function solves an optimization problem whose objective function is
+    the sum of two convex functions.
 
-    * *solver* : solver. It is an object who must implement the :func:`pre`,
-      :func:`algo` and :func:`post` methods.
-    * *f1* : first convex function to minimize. It is an object who must
-      implement the :func:`eval` method. The :func:`grad` and / or :func:`prox`
-      methods are required by some solvers. Please refer to the documentation
-      of the solver.
-    * *f2* : second convex function to minimize, with a :math:`\\beta`
-      Lipschitz continuous gradient. It is an object who must implement the
-      :func:`eval` method. The :func:`grad` and / or :func:`prox` methods are
-      required by some solvers. Please refer to the documentation of
-      the solver.
-    * *x0* : starting point of the algorithm
+    This function minimizes the objective function
+    :math:`f(x) = f_1(x) + f_2(x)`, i.e. solves
+    :math:`\arg\!\min_x f_1(x) + f_2(x)` for
+    :math:`x \in \mathbb{R}^N` using whatever algorithm.
+    Returns a dictionary with the solution and some informations about
+    the algorithm execution.
 
-    **Returns**
+    Parameters
+    ----------
+    solver : solver object
+        the solver algorithm. It is an object who must implement the
+        :func:`pre`, :func:`algo` and :func:`post` methods.
+    f1 : func object
+        first convex function to minimize. It is an object who must implement
+        the :func:`eval` method. The :func:`grad` and / or :func:`prox` methods
+        are required by some solvers. Please refer to the documentation of the
+        considered solver.
+    f2 : func object
+        second convex function to minimize, with a :math:`\\beta`
+        Lipschitz continuous gradient. It is an object who must implement the
+        :func:`eval` method. The :func:`grad` and / or :func:`prox` methods are
+        required by some solvers. Please refer to the documentation of
+        the considered solver.
+    x0 : array_like
+        starting point of the algorithm, :math:`x_0 \in \mathbb{R}^N`
+    relTol : float, optional
+        the relative tolerance stopping criterion. The algorithm stops when
+        :math:`\\frac{n(k)-n(k-1)}{n(k)}<reltol` where
+        :math:`n(k)=f(x)=f_1(x)+f_2(x)` is the objective function at iteration
+        :math:`k`. Default is :math:`10^{-3}`.
+    absTol : float, optional
+        the absolute tolerance stopping criterion. The algorithm stops when
+        :math:`n(k)<abstol`. Default is 0.
+    maxIter : int, optional
+        the maximum number of iterations. Default is 200.
+    verbosity : {'low', 'high', 'none'}, optional
+        'none' for no log, 'low' to print main steps, 'high' to print all
+        steps. Default is 'low'.
 
-    * *sol* : solution
-    * *info* : informations about the algorithm execution
+    Returns
+    -------
+    sol : ndarray
+        solution
+    algo : str
+        used algorithm
+    niter : int
+        number of iterations
+    time : float
+        execution time in seconds
+    eval : float
+        final evaluation of the objective function :math:`f(x)`
+    crit : {'max_it', 'abs_tol', 'rel_tol'}
+        used stopping criterion. 'max_it' if the maximum number of iterations
+        is reached, 'abs_tol' if the objective function value is smaller than
+        `absTol`, 'rel_tol' if the relative objective function improvement was
+        smaller than `relTol`
+    rel : float
+        relative objective improvement at convergence
+    objective : ndarray
+        successive evaluations of the objective function at each iteration
+
+    Examples
+    --------
+
+    Basic example :
+
+    >>> import pyunlocbox
+    >>> solver1 = pyunlocbox.solvers.forward_backward('FISTA', gamma=2)
+    >>> f1 = None
+    >>> f2 = None
+    >>> x0 = None
+    >>> sol, info, objective = pyunlocbox.solvers.solve(solver1, f1, f2, x0)
+    0
     """
 
     # Common initialization.
-    curNorm = f1.eval(x0) + f2.eval(x0)
-# convergence test
-    stop = False
-    niter = 0
-    relNorm = 0
-    crit = 'MAXIT'
+    if relTol < 0 or absTol < 0 or maxIter < 0:
+        raise ValueError('Parameters should be positive numbers.')
+    if verbosity not in ['none', 'low', 'high']:
+        raise ValueError('Verbosity should be either none, low or high.')
+
+    startTime = time.time()
+    objective = [f1.eval(x0) + f2.eval(x0)]
+    stopCrit = None
+    nIter = 0
 
     # Solver specific initialization.
-    solver.pre(x0)
+    solver.pre(x0, verbosity)
 
-    while not stop:
+    while not stopCrit:
 
-        if solver.verbosity == 'high':
-            print('Iteration ' + str(niter))
+        nIter += 1
 
         # Solver iterative algorithm.
-        solver.algo(f1, f2)
+        solver.algo(f1, f2, verbosity)
 
-        # Verify stopping criterion.
-        curNorm = f1.eval(solver.sol) + f2.eval(solver.sol)
+        objective.append(f1.eval(solver.sol) + f2.eval(solver.sol))
 
-# convergence test
+        if objective[-1] == 0:
+            if verbosity in ['low', 'high']:
+                print('WARNING: objective function is equal to 0 ! '
+                      'Adding some epsilon to continue.')
+            # np.spacing(1.0) is equivalent to matlab eps = eps(1.0)
+            objective[-1] = np.spacing(1.0)
+
+        relative = (objective[-1] - objective[-2]) / objective[-1]
+
+        # Verify stopping criteria.
+        if objective[-1] < absTol:
+            stopCrit = 'ABS_TOL'
+        elif abs(relative) < relTol:
+            stopCrit = 'REL_TOL'
+        elif nIter >= maxIter:
+            stopCrit = 'MAX_IT'
+        elif objective[-1] > objective[-2]:
+            stopCrit = 'OBJ_INC'
+
 # post process
-        if solver.verbosity == 'high':
-            print('||f1 + f2|| = ' + str(curNorm) +
-                  ', relative norm : ' + str(relNorm))
+        if verbosity == 'high':
+            print('Iteration %d : objective = %f, relative = %f'
+                  % (nIter, objective[-1], relative))
 
     # Solver specific post-processing.
-    solver.post()
+    solver.post(verbosity)
 
-    # Final logging.
-    if solver.verbosity in ['high', 'low']:
-        print('Solution found (in ' + str(niter) + ' iterations) : ||f|| = ' +
-              str(curNorm) + ', relative norm : ' + str(relNorm) +
-              ', stopping criterion : ' + str(crit))
+    if verbosity in ['high', 'low']:
+        print('Solution found in %d iterations :' % (nIter))
+        print('\tobjective function f(sol) = %f' % (objective[-1]))
+        print('\trelative objective last improvement : %f' % (relative))
+        print('\tstopping criterion : %s' % (stopCrit))
 
-    return solver.sol
+    # Returned dictionary.
+    result = {'sol':       solver.sol,
+              'algo':      solver.__class__.__name__,
+              'niter':     nIter,
+              'time':      time.time() - startTime,
+              'eval':      objective[-1],
+              'objective': objective,
+              'crit':      stopCrit,
+              'rel':       relative}
+
+    return result
 
 
-class solver:
+class solver(object):
+    r"""
+    This class defines the solver object interface.
+
+    This class defines the interface of a solver object intended to be passed
+    to :func:`solve`. It is intended to be a base class for standard solvers
+    which will implement the required methods. It can also be instantiated by
+    user code and dynamically modified for rapid testing. This class also
+    defines the generic attributes of all solver objects.
+
+    Parameters
+    ----------
+    gamma : float
+        the step size. This parameter is upper bounded by
+        :math:`\frac{1}{\beta}` where :math:`f_2` is :math:`\beta` Lipschitz
+        continuous. Default is 1.
     """
-    This class defines a solver object to be passed to :func:`solve`. It is
-    intended to be a base class for standard solvers which will implement
-    the required methods. It can also be instantiated by user code and
-    dynamically modified for rapid testing.
 
-    This class also defines the generic attributes of the solver objects.
-
-    **Attributes :**
-
-    * *gamma* : the step size. This parameter is upper bounded by
-      :math:`\\frac{1}{\\beta}` where :math:`f_2` is :math:`\\beta` Lipschitz
-      continuous. Default is 1.
-    * *reltol* : the relative tolerance stopping criterion. The algorithm stops
-      when :math:`\\frac{n(t)-n(t-1)}{n(t)}<reltol` where
-      :math:`n(t)=f_1(x)+f_2(x)` is the objective function at iteration
-      :math:`t`. Default is :math:`10^{-3}`.
-    * *abstol* : the absolute tolerance stopping criterion. The algorithm stops
-      when :math:`n(t)<abstol`. Default is 0.
-    * *maxit* : maximum number of iterations. Default is 200.
-    * *verbosity* : 'none' for no log, 'low' to print main steps, 'high' to
-      print all steps.
-    """
-
-    def __init__(self, gamma=1, reltol=10**-3, abstol=0, maxit=200,
-                 verbosity='low'):
+    def __init__(self, gamma=1):
+        if gamma < 0:
+            raise ValueError('Gamma should be a positive number.')
         self.gamma = gamma
-        self.reltol = reltol
-        self.abstol = abstol
-        self.maxit = maxit
-        if verbosity not in ['none', 'low', 'high']:
-            raise ValueError('verbosity should be either none, low or high')
-        self.verbosity = verbosity
 
-    def pre(self, x0):
+    def pre(self, x0, verbosity):
         """
-        Solver specific initialization.
+        Solver specific initialization. See parameters documentation in
+        :func:`pyunlocbox.solvers.solve` documentation.
         """
         raise NotImplementedError("Class user should define this method.")
 
-    def algo(self, f1, f2):
+    def algo(self, f1, f2, verbosity):
         """
-        Solver iterative algorithm.
+        Solver iterative algorithm. See parameters documentation in
+        :func:`pyunlocbox.solvers.solve` documentation.
         """
         raise NotImplementedError("Class user should define this method.")
 
-    def post(self):
+    def post(self, verbosity):
         """
-        Solver specific post-processing.
+        Solver specific post-processing. See parameters documentation in
+        :func:`pyunlocbox.solvers.solve` documentation.
         """
         raise NotImplementedError("Class user should define this method.")
 
 
 class forward_backward(solver):
-    """
-    Forward-backward splitting algorithm.
-
-    This algorithm requires ``f1`` to implement the :func:`prox` method and
-    ``f2`` to implement the :func:`grad` method.
-
-    **Usage**
-
-    ``solver1 = solvers.forward_backward(...)
-    sol = solvers.solve(solver1, f1, f2, x0)``
-    TODO: using doctest
-
-    **Attributes**
-
-    * *method* : the method used to solve the problem. It can be 'FISTA' or
-      'ISTA'. Default is 'FISTA'.
-    * *lamb* : the update term weight for ISTA. It should be between 0 and 1.
-      Default is 1.
+    r"""
+    Forward-backward proximal splitting algorithm.
 
     See generic attributes descriptions of the base class :class:`solver`.
 
-    **Description**
+    Parameters
+    ----------
+    method : {'FISTA', 'ISTA'}, optional
+        the method used to solve the problem.  It can be 'FISTA' or 'ISTA'.
+        Default is 'FISTA'.
+    lamb : float, optional
+        the update term weight for ISTA.  It should be between 0 and 1. Default
+        is 1.
 
-    ``forward_backward`` solves :math:`arg \min_x f_1(x) + f_2(x)`
-    for :math:`x \in \mathbb{R}^N` where :math:`x` is the variable.
+    Notes
+    -----
+    This algorithm requires `f1` to implement the :meth:`prox` method and `f2`
+    to implement the :meth:`grad` method.
+
+    Examples
+    --------
+    >>> import pyunlocbox
+    >>> solver1 = pyunlocbox.solvers.forward_backward('FISTA', gamma=2)
+    >>> f1 = None
+    >>> f2 = None
+    >>> x0 = None
+    >>> sol = pyunlocbox.solvers.solve(solver1, f1, f2, x0)
+    0
     """
 
     def __init__(self, method='FISTA', lamb=1, *args, **kwargs):
+
         solver.__init__(self, *args, **kwargs)
 
-        if method != 'FISTA' or method != 'ISTA':
-            raise ValueError('method should be FISTA or ISTA')
+        if method not in ['FISTA', 'ISTA']:
+            raise ValueError('The method should be FISTA or ISTA.')
         self.method = method
+
         if lamb < 0 or lamb > 1:
-            raise ValueError('lamb is bounded by 0 and 1')
+            raise ValueError('Lambda is bounded by 0 and 1.')
         self.lamb = lamb
 
-    def pre(self, x0):
+    def pre(self, x0, verbosity):
         """
         Algorithm initialization.
         """
-        if self.verbosity == 'high':
-            print('Selected algorithm : ' + str(self.method))
+        if verbosity == 'high':
+            print('Selected algorithm : %s' % (self.method))
 
         # ISTA and FISTA initialization.
         self.sol = np.array(x0)
@@ -182,13 +266,12 @@ class forward_backward(solver):
         self.un = np.array(x0)
         self.tn = 1.
 
-    def algo(self, f1, f2):
+    def algo(self, f1, f2, verbosity):
         """
         Iterative ISTA or FISTA algorithm.
         """
         if self.method == 'ISTA':
             yn = self.sol - self.gamma * f2.grad(self.sol)
-#FIXME gamma is not the same as in matlab
             self.sol += self.lamb * (f1.prox(yn, self.gamma) - self.sol)
         elif self.method == 'FISTA':
             xn = f1.prox(self.un - self.gamma * f2.grad(self.un), self.gamma)
@@ -197,9 +280,10 @@ class forward_backward(solver):
             self.sol = xn
             self.tn = tn1
         else:
-            raise ValueError('method should be FISTA or ISTA')
+            raise ValueError('The method should be FISTA or ISTA.')
 
-    def post(self):
+    def post(self, verbosity):
         """
         No post-processing.
         """
+        pass
