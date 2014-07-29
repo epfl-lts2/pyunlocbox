aaca3ce6e2ce2848c867558426119854463d8b50
Michaël Defferrard <michael.defferrard@epfl.ch>
2014-07-29 16:22:00 +0200

    Dummy function object returns correct proximal operator

--- pyunlocbox/tests/test_functions.py
+++ pyunlocbox/tests/test_functions.py
@@ -55,11 +55,11 @@ class FunctionsTestCase(unittest.TestCase):
         f = functions.dummy()
         self.assertEqual(f.eval(34), 0)
         nptest.assert_array_equal(f.grad(34), [0])
-        nptest.assert_array_equal(f.prox(34, 1), [17])
+        nptest.assert_array_equal(f.prox(34, 1), [34])
         x = [34, 2, 1.0, -10.2]
         self.assertEqual(f.eval(x), 0)
         nptest.assert_array_equal(f.grad(x), np.zeros(len(x)))
-        nptest.assert_array_equal(f.prox(x, 1), np.array(x)/2.)
+        nptest.assert_array_equal(f.prox(x, 1), x)
 
     def test_norm_l2(self):
         """
