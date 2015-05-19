1decb6e7adc504ac158cd1b6a136229eb651e8c4
Michaël Defferrard <michael.defferrard@epfl.ch>
2015-05-19 23:47:13 +0200

    generalized_forward_backward: more comprehensive unit tests

--- pyunlocbox/tests/test_solvers.py
+++ pyunlocbox/tests/test_solvers.py
@@ -215,28 +215,6 @@ class FunctionsTestCase(unittest.TestCase):
         self.assertEqual(ret['crit'], 'ATOL')
         self.assertEqual(ret['niter'], 21)
 
-    def test_generalized_forward_backward(self):
-        """
-        Test generalized forward-backward algorithm with L1-norm, L2-norm, 
-        and dummy functions. Test the effect of step and lambda parameters.
-        """
-        y = [4, 5, 6, 7]
-        x0 = np.zeros(len(y))
-        # Smaller step size and update rate --> slower convergence.
-        solver = solvers.generalized_forward_backward(step=.8, lambda_=.5)
-        param = {'x0': x0, 'solver': solver}
-        param['atol'] = 1e-5
-        param['verbosity'] = 'NONE'
-
-        # L1-norm prox and L2-norm gradient.
-        f1 = functions.norm_l1(y=y, lambda_=1.0)
-        f2 = functions.norm_l2(y=y, lambda_=0.8)
-        sol = [3.99999825, 4.9999979, 5.99999756, 6.99999723]
-        ret = solvers.solve([f1, f2], **param)
-        nptest.assert_allclose(ret['sol'], sol)
-        self.assertEqual(ret['crit'], 'ATOL')
-        self.assertEqual(ret['niter'], 21)
-
     def test_douglas_rachford(self):
         """
         Test douglas-rachford solver with L1-norm, L2-norm and dummy functions.
@@ -261,6 +239,40 @@ class FunctionsTestCase(unittest.TestCase):
         nptest.assert_allclose(ret['sol'], y)
         self.assertEqual(ret['crit'], 'RTOL')
         self.assertEqual(ret['niter'], 4)
+
+    def test_generalized_forward_backward(self):
+        """
+        Test the generalized forward-backward algorithm.
+        """
+        y = [4, 5, 6, 7]
+        x0 = np.zeros(len(y))
+        L = 4
+        solver = solvers.generalized_forward_backward(step=.9/L, lambda_=.8)
+        params = {'x0': x0, 'solver':solver, 'verbosity': 'NONE'}
+
+        # Functions .
+        f1 = functions.norm_l1(y=y, lambda_=.7)  # Smooth.
+        f2 = functions.norm_l2(y=y, lambda_=L/2.)  # Non-smooth.
+
+        # Solve with 1 smooth and 1 non-smooth.
+        ret = solvers.solve([f1, f2], **params)
+        nptest.assert_allclose(ret['sol'], y)
+
+        # Solve with 1 smooth.
+        ret = solvers.solve([f1], **params)
+        nptest.assert_allclose(ret['sol'], y)
+
+        # Solve with 1 non-smooth.
+        ret = solvers.solve([f2], **params)
+        nptest.assert_allclose(ret['sol'], y)
+
+        # Solve with 1 smooth and 2 non-smooth.
+        ret = solvers.solve([f1, f2, f2], **params)
+        nptest.assert_allclose(ret['sol'], y)
+
+        # Solve with 2 smooth and 2 non-smooth.
+        ret = solvers.solve([f2, f1, f2, f1], **params)
+        nptest.assert_allclose(ret['sol'], y)
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(FunctionsTestCase)
