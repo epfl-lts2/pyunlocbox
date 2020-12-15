a37f9de91db3aa22e509f96364666dbe3ce35490
Michaël Defferrard <michael.defferrard@epfl.ch>
2020-12-15 22:56:04 +0100

    sphinxcontrib-bibtex v2 requires bibfiles

--- doc/conf.py
+++ doc/conf.py
@@ -7,7 +7,6 @@ extensions = [
         'sphinx.ext.autosummary',
         'sphinx.ext.mathjax',
         'sphinx.ext.inheritance_diagram',
-        'sphinxcontrib.bibtex',
 ]
 
 extensions.append('sphinx.ext.autodoc')
@@ -38,6 +37,9 @@ plot_working_directory = '.'
 
 extensions.append('sphinx_copybutton')
 copybutton_prompt_text = ">>> "
+
+extensions.append('sphinxcontrib.bibtex')
+bibtex_bibfiles = ['references.bib']
 
 exclude_patterns = ['_build']
 source_suffix = '.rst'
